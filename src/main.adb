fd4713e7e0a4718b6a0ee91e076bb90ffc944f95
Vincent Deville <vincent.deville@epita.fr>
2020-01-31 18:57:32 +0100

    Remove unused code

--- src/main.adb
+++ src/main.adb
@@ -1,41 +1,5 @@
-------------------------------------------------------------------------------
---                                                                          --
---                     Copyright (C) 2015-2016, AdaCore                     --
---                                                                          --
---  Redistribution and use in source and binary forms, with or without      --
---  modification, are permitted provided that the following conditions are  --
---  met:                                                                    --
---     1. Redistributions of source code must retain the above copyright    --
---        notice, this list of conditions and the following disclaimer.     --
---     2. Redistributions in binary form must reproduce the above copyright --
---        notice, this list of conditions and the following disclaimer in   --
---        the documentation and/or other materials provided with the        --
---        distribution.                                                     --
---     3. Neither the name of the copyright holder nor the names of its     --
---        contributors may be used to endorse or promote products derived   --
---        from this software without specific prior written permission.     --
---                                                                          --
---   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    --
---   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      --
---   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  --
---   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   --
---   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, --
---   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       --
---   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  --
---   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  --
---   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    --
---   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  --
---   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
---                                                                          --
-------------------------------------------------------------------------------
-
---  A very simple draw application.
---  Use your finger to draw pixels.
 
 with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
---  The "last chance handler" is the user-defined routine that is called when
---  an exception is propagated. We need it in the executable, therefore it
---  must be somewhere in the closure of the context clauses.
 
 with STM32.Board;           use STM32.Board;
 with HAL.Bitmap;            use HAL.Bitmap;
@@ -59,23 +23,10 @@ is
   BPM_Value : Integer;
   BPM_Valid : Boolean := True;
 begin
-  --  Initialize LCD
   Display.Initialize(Landscape);
   Display.Initialize_Layer (Text_Layer, ARGB_8888, 0, 0, 320, 120);
   Display.Initialize_Layer (Graph_Layer, ARGB_8888, 0, 120, 320, 120);
   Init(Cardiac_Info);
-  --  Initialize touch panel
-
-  --  Initialize button
-  --User_Button.Initialize;
-
-  --  Clear LCD (set background)
-
-  --  The application: set pixel where the finger is (so that you
-  --  cannot see what you are drawing).
-  --STM32.Board.Initialize_LEDs
-
-  --TestADCProc;
   Initialize (COM);
   Configure (COM, Baud_Rate => 9_600);
   Initialize_LEDs;
@@ -115,5 +66,4 @@ exception
       loop
          null;
       end loop;
-     
 end Main;
--- src/testadc.ads
+++
@@ -1,5 +0,0 @@
-package testadc is
-   
-   procedure TestADCProc;
-
-end testadc;
